48cc6c6ead594cb71ab8ea2fa387133091cedd29
adielCalixto <adielsemr@protonmail.com>
2022-04-19 20:41:06 -0300

    fixed broken import on backend

--- mercantree/urls.py
+++ mercantree/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import urls as users_urls
 from rest_framework import routers
 from users import views
 
