f84dc64b2a14243f6341c5c2f1fabf635a386158
Weinny <joseweinny.10@gmail.com>
2022-04-25 11:54:43 -0300

    added products app

--- mercantree/urls.py
+++ mercantree/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from users import views
+from users import views as UserViews
+from products import views as ProductViews
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'users', UserViews.UserViewSet)
+router.register(r'products', ProductViews.ProductViewSet)
+router.register(r'suppliers', ProductViews.SupplierViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
---
+++ products/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    cnpj = models.CharField(max_length=14, null=True)
+    responsable = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    barcode = models.CharField(max_length=255)
+    expires_at = models.DateTimeField('date of expiration')
+    price = models.FloatField()
+    category = models.CharField(max_length=100)
+    supplier_id = models.ForeignKey(Supplier, on_delete=models.CASCADE, null=True)
