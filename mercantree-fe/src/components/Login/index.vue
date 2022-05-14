ed2b8e37b4220948f3c693ba06e064971dd8c645
adielCalixto <adielsemr@protonmail.com>
2022-05-14 15:49:29 -0300

    login page refactored

--- mercantree-fe/src/components/Login/index.vue
+++ mercantree-fe/src/components/Login/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="max-w-md mx-auto my-16 p-4">
-        <div class="text-center mb-8">
+        <div class="text-center py-2 bg-primary text-gray-100">
             <h1 class="text-xl">Faça login no sistema</h1>
         </div>
 
-        <div class="p-4 bg-white shadow-lg">
+        <form @submit.prevent="login" class="p-4 bg-base-100 shadow-lg">
             <div class="form-control w-full">
                 <label class="label">
                     <span class="label-text">Email</span>
@@ -17,10 +17,10 @@
                 </label>
                 <input type="password" v-model="data.password" placeholder="senha" class="input input-bordered w-full">
             </div>
-            <div class="form-control w-full">
-                <button @click="login" class="btn btn-success btn-outline btn-block">Entrar</button>
+            <div class="form-control items-end">
+                <button type="submit" class="btn btn-secondary btn-sm w-1/3 btn-outline">Entrar</button>
             </div>
-        </div>
+        </form>
     </div>
 </template>
 
@@ -28,33 +28,35 @@
 
 import { useStore } from '../../stores/auth'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default {
     name: 'Login',
     setup() {
         const store = useStore()
+        const router = useRouter()
 
         const data = ref({
             email: '',
             password: '',
         })
 
+        const login = () => {
+            store.loginUser({username: data.value.email, password: data.value.password})
+            .then(() => {
+                router.push('/') 
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
+
         return {
             store,
+            login,
             data,
         }
     },
-    methods: {
-        login: function() {
-            this.store.loginUser({username: this.data.email, password: this.data.password})
-            .then(response => {
-                this.$router.push('/') 
-            })
-            .catch(error => {
-                
-            })
-        }
-    }
 }
 </script>
 
