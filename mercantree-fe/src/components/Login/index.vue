c80b2322e620b0f8539396cee836e25818a0d3b4
EMadu004 <edumariacarvpd@gmail.com>
2022-06-01 13:53:07 -0300

    login page

--- mercantree-fe/src/components/Login/index.vue
+++ mercantree-fe/src/components/Login/index.vue
@@ -1,44 +1,46 @@
 <template>
-    <div class="max-w-md mx-auto py-16 p-4">
-        <div class="text-center py-2 bg-primary text-gray-100">
-            <h1 class="text-xl">Faça login no sistema</h1>
+    <div class="bg-base-200 h-screen py-16">
+        <div class="max-w-md mx-auto p-4 bg-primary rounded-box">
+            <div class="text-center py-2 text-neutral">
+                <h1 class="text-xl font-bold">Mercan<span class="text-green-500 font-bold">Tree</span> | Login</h1>
+            </div>
+
+            <form @submit.prevent="login" class="p-4">
+                <div class="form-control w-full">
+                    <label class="label">
+                        <span class="label-text font-semibold">Email</span>
+                    </label>
+
+                    <input
+                    type="email"
+                    v-model="state.email"
+                    placeholder="email"
+                    class="input input-bordered w-full">
+
+                    <div class="badge badge-ghost rounded-none mt-1 ml-auto gap-2" v-if="v$.email.$error">
+                        {{ v$.email.$errors[0].$message }}
+                    </div>
+                </div>
+                <div class="form-control w-full my-4">
+                    <label class="label">
+                        <span class="label-text font-semibold">Senha</span>
+                    </label>
+
+                    <input
+                    type="password"
+                    v-model="state.password"
+                    placeholder="senha"
+                    class="input input-bordered w-full">
+
+                    <div class="badge badge-ghost rounded-none mt-1 ml-auto gap-2" v-if="v$.password.$error">
+                        {{ v$.password.$errors[0].$message }}
+                    </div>
+                </div>
+                <div class="form-control items-end">
+                    <button type="submit" class="btn btn-success text-primary btn-sm w-1/3 ">Entrar</button>
+                </div>
+            </form>
         </div>
-
-        <form @submit.prevent="login" class="p-4 bg-base-100 shadow-lg">
-            <div class="form-control w-full">
-                <label class="label">
-                    <span class="label-text">Email</span>
-                </label>
-
-                <input
-                type="email"
-                v-model="state.email"
-                placeholder="email"
-                class="input input-bordered w-full">
-
-                <div class="badge badge-ghost rounded-none mt-1 ml-auto gap-2" v-if="v$.email.$error">
-                    {{ v$.email.$errors[0].$message }}
-                </div>
-            </div>
-            <div class="form-control w-full my-4">
-                <label class="label">
-                    <span class="label-text">Senha</span>
-                </label>
-
-                <input
-                type="password"
-                v-model="state.password"
-                placeholder="senha"
-                class="input input-bordered w-full">
-
-                <div class="badge badge-ghost rounded-none mt-1 ml-auto gap-2" v-if="v$.password.$error">
-                    {{ v$.password.$errors[0].$message }}
-                </div>
-            </div>
-            <div class="form-control items-end">
-                <button type="submit" class="btn btn-secondary btn-sm w-1/3 btn-outline">Entrar</button>
-            </div>
-        </form>
     </div>
 </template>
 
