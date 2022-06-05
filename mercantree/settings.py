d2cf018804a573bbf557eb4aaa97c7e56beb5ae7
adielCalixto <adielsemr@protonmail.com>
2022-06-05 13:01:48 -0300

    MercanTree 1.0 Release

--- mercantree/settings.py
+++ mercantree/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n&c2(hgl($ug-&%&vp2g-(0m96aa1t#7*h_38o*5qmsuc!e$3#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
