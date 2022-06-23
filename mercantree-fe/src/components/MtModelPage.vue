55fd6e0ccd3a23d2a49bde2803b2d4ec5dcac150
adielCalixto <adielsemr@protonmail.com>
2022-06-23 20:06:58 -0300

    min height on pages

--- mercantree-fe/src/components/MtModelPage.vue
+++ mercantree-fe/src/components/MtModelPage.vue
@@ -1,6 +1,6 @@
 <template>
     <Suspense>
-        <div class="md:ml-48 mx-auto container max-w-full relative overflow-y-hidden overflow-x-auto">
+        <div class="md:ml-48 mx-auto container max-w-full min-h-screen relative overflow-y-hidden overflow-x-auto">
             <div class="text-sm breadcrumbs mb-4">
                 <ul>
                     <li v-for="i in $route.matched.length - 2">
